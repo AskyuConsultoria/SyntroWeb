95faeec0c74288de2836cdd8c4d18a1d165df548
kayky <kayky.pedroso@sptech.school>
2025-09-12 11:19:25 -0300

    Chore: mudança de rotas do token

--- src/services/passwordRecoveryService.js
+++ src/services/passwordRecoveryService.js
@@ -3,7 +3,7 @@ import apiFetch from "../services/api.js";
 const passwordRecoveryService = {
   esqueceuSenha: async (email) => {
     console.log("Email enviado ao service:", email);
-    return await apiFetch("/auth/esqueceu-senha", {
+    return await apiFetch("/user/esqueceu-senha", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email }),
@@ -11,7 +11,7 @@ const passwordRecoveryService = {
   },
 
   resetarSenha: async (token, novaSenha) => {
-    return await apiFetch("/auth/resetar-senha", {
+    return await apiFetch("/user/resetar-senha", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ token, novaSenha }),
