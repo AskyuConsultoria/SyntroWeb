a2230ba8a9339d007f4e8b0a36ceb745d56ac9d8
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2025-12-09 12:24:46 -0300

    feat: Alteração de senha

---
+++ src/public/components/passwordChange/password.js
@@ -0,0 +1,97 @@
+let modalLoadedP = false;
+
+async function loadModal() {
+    if (modalLoadedP) return;
+
+    const html = await fetch("/public/components/passwordChange/password.html").then(r => r.text());
+
+    document.body.insertAdjacentHTML("beforeend", html);
+
+    setTimeout(() => {
+        initializeModalP();
+    }, 50);
+
+    modalLoadedP = true;
+}
+
+function initializeModalP() {
+    window.modalOverlay = document.getElementById("globalModal");
+    window.modalMessage = document.getElementById("modalMessage");
+    window.modalIcon = document.getElementById("modalIcon");
+    window.modalCloseBtn = document.getElementById("modalCloseBtn");
+    window.modalFinishBtn = document.getElementById("modalFinishBtn");
+    window.SenhaAntiga = document.getElementById("SenhaAntiga");
+    window.NovaSenha = document.getElementById("NovaSenha");
+
+    modalCloseBtn.addEventListener("click", closeModalP);
+
+    modalFinishBtn.addEventListener("click", () => {
+        const autentificado = autentificar(SenhaAntiga.value)
+        if(autentificado){
+        const newPassword = NovaSenha.value;
+        mudarsenha(newPassword);
+        closeModalP();
+        showModal("Senha alterada com sucesso!", "success");
+        setTimeout(() => {
+            window.location.href = "/";
+        }, 2000);
+    } else{
+        showModal("Senha antiga incorreta!", "error");
+    }
+    });
+    
+    modalOverlay.addEventListener("click", (event) => {
+        if (event.target === modalOverlay) {
+            closeModalP();
+        }
+    });
+}
+
+function showPasswordChanger() {
+    if (!modalLoadedP) {
+        loadModal().then(() => {
+            setTimeout(() => showPasswordChanger(), 50);
+        });
+        return;
+    }
+
+    // Agora tem certeza que tudo está carregado
+    const modalOverlay = document.getElementById("globalModal");
+    const modalMessage = document.getElementById("modalMessage");
+    const modalIcon = document.getElementById("modalIcon");
+
+    modalOverlay.classList.remove("hidden");
+}
+
+
+function closeModalP() {
+    modalOverlay.classList.add("hidden");
+}
+
+async function autentificar(senha){
+    var usuario = sessionStorage.getItem("usuarioLogado");
+    email = JSON.parse(usuario).email;
+    const resposta = await fetch('http://localhost:8080/syntro/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        login: email,
+        password: senha
+      })
+    });
+    if (resposta.ok) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function mudarsenha(novaSenha){
+    var usuario = sessionStorage.getItem("usuarioLogado");
+    id = JSON.parse(usuario).id;
+    fetch(`http://localhost:8080/syntro/user/${id}?novaSenha=${novaSenha}`, {
+      method: 'PUT'
+    });
+}
