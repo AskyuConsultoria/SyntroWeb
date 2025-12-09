489130238c0c5a782a7dcb5e816ce6fabd448e2e
KaykyBelarmino <kaykypedroso@sptech.school>
2025-12-09 10:53:30 -0300

    Merge: merge das funcionalidades de dash, empresas e cadastros

--- src/public/components/sidebar.js
+++ src/public/components/sidebar.js
@@ -43,7 +43,7 @@ class Sidebar extends HTMLElement {
         { text: "Empresas", icon: "/assets/building-3.svg", href: ["/empresas"] },
         { text: "Cadastros", icon: "/assets/folder-add2.svg", href: ["/cadastros/home"] },
         { text: "Permissões", icon: "/assets/user-search.svg", href: ["/permissionamento"] },
-        { text: "Controle", icon: "/assets/icons.svg", href: ["#"] },
+        { text: "Controle", icon: "/assets/icons.svg", href: ["/dashboard"] },
         { text: "Ajuda", icon: "/assets/info.svg", href: ["https://app.pipefy.com/organizations/301284564/portal?form=1bbad609-f3d5-4690-9d92-62cc8b34bc9f"]},
       ]
     }
