a22e0ce32b2444156ae8a40fab02a97582c968cf
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2025-12-09 15:59:36 -0300

    feat: listagem de emissores

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.get("/empresas", (req, res) => {
     res.sendFile(path.join(__dirname, "src/public/empresas/lista_empresas.html"));
 });
 
+app.get("/empresas/detalhes", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/public/empresas/empresa-details.html"));
+});
+
 app.get("/servicos", (req, res) => {
     res.sendFile(path.join(__dirname, "src/public/servicos/servicos.html"));
 });
